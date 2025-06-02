baf421909ce0349fa964a1644a2ac62415c19f41
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-03 01:03:15 +0900

    Update: ps1

--- ps/amd64_win_autorun.ps1
+++ ps/amd64_win_autorun.ps1
@@ -1,7 +1,19 @@
+# Recomended PowerShell Core Version: 7.5.0 or later
+Write-Host "Building TabDock for Windows amd64..."
+Write-Host "Removing old build files..."
 Remove-Item ./dist/tabdock_win_amd64.exe -Force
+
+Write-Host "Setting environment variables for GOOS and GOARCH..."
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
+
+Write-Host "Building the application..."
 go build -o ./dist/tabdock_win_amd64.exe main.go log.go
+
+Write-Host "Build complete. Cleaning up environment variables..."
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
+
+Write-Host "Build successful! The executable is located at ./dist/tabdock_win_amd64.exe"
+Write-Host "Running the application..."
 ./dist/tabdock_win_amd64.exe
