9d55b0dd99a4ba02e07d401eeef748d739c6cdbd
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-26 23:27:06 +0900

    権利証明

--- home/weather.js
+++ home/weather.js
@@ -1,3 +1,8 @@
+// 2025 TabDock: darui3018823 All rights reserved.
+// All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
+// Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
+// This code Version: 2.6.0_r1
+
 let weatherDetailData = [];
 let weatherData = null;
 let weatherDetailParsedData = {};
