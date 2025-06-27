184a634266519978d06754f6afa9b0c211d8aeb0
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-27 23:45:31 +0900

    jsをupdate

--- home/status.js
+++ home/status.js
@@ -1,9 +1,10 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 2.7.0_status-r1
+// This code Version: 2.7.1_status-r2
 
 document.getElementById("openStatusModal").addEventListener("click", () => {
+    document.getElementById("menuModal").classList.add("hidden");
     document.getElementById("statusModal").classList.remove("hidden");
 
     // 各APIの確認
