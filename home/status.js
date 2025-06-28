291408d625ebe0c42a40c8685b54b4ac15a8fd7b
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-28 18:38:59 +0900

    fix2

--- home/status.js
+++ home/status.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 2.8.4_status-r7
+// This code Version: 2.8.7_status-r2
 
 function checkApi(endpoint, labelId, method = "HEAD") {
     const start = performance.now();
