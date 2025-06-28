90fcb619456a845e49aa6a6a725f5c64bc55810c
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-28 13:11:57 +0900

    記載忘れ

--- home/tabdock_about.js
+++ home/tabdock_about.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 2.7.5_about-r1
+// This code Version: 2.7.5_about-r2
 
 document.getElementById("openAboutModal").addEventListener("click", () => {
     document.getElementById("menuModal").classList.add("hidden");
