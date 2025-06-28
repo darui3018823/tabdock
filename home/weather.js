110c75cc279c17466544df70214af1b7b7bb71f1
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-28 18:38:38 +0900

    fix

--- home/weather.js
+++ home/weather.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 2.6.0_weather-r2
+// This code Version: 2.8.7_weather-r3
 
 let weatherDetailData = [];
 let weatherData = null;
