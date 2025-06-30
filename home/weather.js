32ad850e8753ae106dcf08274a25b38c20a26a4b
darui3018823 <n.sumikko.0711@gmail.com>
2025-06-30 18:53:59 +0900

    些細な修正

--- home/weather.js
+++ home/weather.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 2.9.3_weather-r7
+// This code Version: 2.9.3_weather-r7.1
 
 let weatherDetailData = [];
 let weatherData = null;
@@ -24,7 +24,6 @@ async function fetchWeather() {
     const updateEl = document.getElementById("weather-last-update");
     if (updateEl) {
         updateEl.textContent = updateTime;
-        updateEl.classList.remove("hidden");
     }
 
     try {
