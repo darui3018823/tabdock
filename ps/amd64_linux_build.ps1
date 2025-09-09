1e3a6441ec8caff45b6a1222e032ff318332034f
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2025-09-10 01:22:54 +0900

    fix

--- ps/amd64_linux_build.ps1
+++ ps/amd64_linux_build.ps1
@@ -1,19 +1,19 @@
 # Recommended PowerShell Core Version: 7.5.0 or later
 Write-Host "Note: There is no guarantee that this script will work correctly on Linux."
-Write-Host "Building TabDock for Linux arm64..."
+Write-Host "Building TabDock for Linux amd64..."
 Write-Host "Removing old build files..."
-Remove-Item ./dist/tabdock_linux_arm64 -Force
+Remove-Item ./dist/tabdock_linux_amd64 -Force
 
 Write-Host "Setting environment variables for GOOS and GOARCH..."
 $env:GOOS = "linux"
-$env:GOARCH = "arm64"
+$env:GOARCH = "amd64"
 
 Write-Host "Building the application..."
-go build -o ./dist/tabdock_linux_arm64 main.go log.go
+go build -o ./dist/tabdock_linux_amd64 main.go log.go
 
 Write-Host "Build complete. Cleaning up environment variables..."
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
 
-Write-Host "Build successful! The executable is located at ./dist/tabdock_linux_arm64"
+Write-Host "Build successful! The executable is located at ./dist/tabdock_linux_amd64"
 Write-Host "===================================================="
