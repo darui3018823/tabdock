2a9f9d7a56aa23410ed42c7a6ff2891a62867a7d
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2025-09-20 00:46:14 +0900

    Swalの種類を修正

--- home/account.js
+++ home/account.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 4.4.0_acc-r1
+// This code Version: 4.4.0_acc-r2
 
 document.addEventListener("DOMContentLoaded", () => {
     window.onPasskeyLoginSuccess = function(user) {
@@ -776,11 +776,15 @@ function updateUIForLoggedInUser(user) {
     console.log("ログインユーザー:", user);
 
     Swal.fire({
-        title: "ログイン成功",
-        text: `${user.username} としてログインしました。`,
-        icon: "success"
+        toast: true,
+        position: 'top-end',
+        icon: 'success',
+        title: `${user.username} としてログインしました`,
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true
     });
-    
+
     const accManageBtn = document.getElementById("openAccManage");
     if (accManageBtn) {
         accManageBtn.innerHTML = `
