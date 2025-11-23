94dd44b32ff95754ed5722f035de4345e2a18737
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2025-11-23 22:25:51 +0900

    改行文字を追加

--- home/calendar.js
+++ home/calendar.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 5.13.0_calendar-r3
+// This code Version: 5.13.0_calendar-r4
 
 const calendarGrid = document.getElementById("calendarGrid");
 const currentMonthElem = document.getElementById("currentMonth");
