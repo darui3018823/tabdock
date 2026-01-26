e90393d9b34f4bac36e7ff3c3acd518c27d80348
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2026-01-27 01:41:25 +0900

    fix: update version number to 5.16.3_subsc-r1

--- home/subscription.js
+++ home/subscription.js
@@ -1,7 +1,7 @@
 // 2025 TabDock: darui3018823 All rights reserved.
 // All works created by darui3018823 associated with this repository are the intellectual property of darui3018823.
 // Packages and other third-party materials used in this repository are subject to their respective licenses and copyrights.
-// This code Version: 5.15.3_subsc-r1
+// This code Version: 5.16.3_subsc-r1
 
 class SubscriptionManager {
     constructor() {
